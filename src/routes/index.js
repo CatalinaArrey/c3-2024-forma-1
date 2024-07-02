f0ce5f5d6bac4f74418496514e55c38ddb036e83
Catalina Arrey <Catalina.arrey@sansano.usm.cl>
2024-07-01 20:06:11 -0400

    Resolución control 3, Catalina Arrey 202030023-2"

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,13 +1,37 @@
-import Router from 'koa-router'
-import getHealth from './health/health'
-import getCitiesUseCase from '../domain/cities/use_cases/getCities'
+import Router from 'koa-router';
+import getHealth from './health/health';
+import * as getCitiesUseCase from '../domain/cities/use_cases/getCities';
 
-const router = new Router()
+const router = new Router();
 
-router.get('/health', getHealth)
+router.get('/by_country/:country', async (ctx) => {
+  const { country } = ctx.params;
+  if (!/^[a-zA-Z]+$/.test(country)) {
+    ctx.status = 400;
+    ctx.body = { message: 'Solo se aceptan caracteres no numéricos' };
+    return;
+  }
 
-router.get('/api/cities', getCitiesUseCase.getAllCitiesUseCase)
-router.get('/api/cities/by_country/:country', getCitiesUseCase.getCitiesByCountryUseCase)
-router.get('/api/city/:city/country/:country', getCitiesUseCase.getCitiesByCityNameAndCountryUseCase)
+  if (country.length < 3) {
+    ctx.status = 400;
+    ctx.body = { message: "El país/ciudad ingresado debe tener al menos 3 caracteres" };
+    return;
+  }
 
-export default router
+  try {
+    const cities = await getCitiesUseCase.getCitiesByCountryUseCase(ctx);
+    ctx.status = 200;
+    ctx.body = cities;
+  } catch (error) {
+    ctx.status = 500;
+    ctx.body = { error: 'Internal Server Error' };
+  }
+});
+
+router.get('/health', getHealth);
+
+router.get('/api/cities', getCitiesUseCase.getAllCitiesUseCase);
+router.get('/api/cities/by_country/:country', getCitiesUseCase.getCitiesByCountryUseCase);
+router.get('/api/city/:city/country/:country', getCitiesUseCase.getCitiesByCityNameAndCountryUseCase);
+
+export default router;
